6e44fce39935670d418810efe2bc9a898ddf9a71
dhanushrishi <dhanushrishi13@gmail.com>
2022-09-04 13:14:54 +0530

    Architect Asian development

---
+++ src/test/java/qaclickacademy/raw.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+public class raw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("post3");
+		System.out.println("post4");
+		System.out.println("post5");
+		System.out.println("post3");
+		System.out.println("post4");
+		System.out.println("post5");
+		System.out.println("post3");
+		System.out.println("post4");
+		System.out.println("post5");
+	}
+
+}
