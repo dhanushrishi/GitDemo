ab69d30e6b597af136184fbfc55398a2f5f412de
dhanushrishi <dhanushrishi13@gmail.com>
2022-09-04 12:13:08 +0530

    this is commit from asian timezone guy

---
+++ src/test/java/qaclickacademy/restapi.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+public class restapi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("post");
+		System.out.println("post1");
+		System.out.println("post2");
+		
+		
+	}
+
+}
